be3d530e28f862881861693a02300bf7bdd72e5a
VenCKi <kvkirthy@gmail.com>
2014-10-05 23:50:58 +0530

    Initial Commit of Geo Location Sample in Cordova based Hybrid Visual Studio Solution

---
+++ BlankCordovaApp1/scripts/index.js
@@ -0,0 +1,41 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkID=397704
+// To debug code on page load in Ripple or on Android devices/emulators: launch your app, set breakpoints, 
+// and then run "window.location.reload()" in the JavaScript Console.
+(function () {
+    "use strict";
+
+    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+
+    function onDeviceReady() {
+        // Handle the Cordova pause and resume events
+        document.addEventListener( 'pause', onPause.bind( this ), false );
+        document.addEventListener( 'resume', onResume.bind( this ), false );
+        getGeoLocation();
+        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
+    };
+
+    function onPause() {
+        // TODO: This application has been suspended. Save application state here.
+    };
+
+    function onResume() {
+        // TODO: This application has been reactivated. Restore application state here.
+    };
+})();
+
+
+
+function getGeoLocation() {
+    if (navigator && navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(setLongitudeAndLatitude);
+    } 
+}
+
+function setLongitudeAndLatitude(position) {
+    var longitudeElm = document.getElementById("longitude");
+    longitudeElm.innerText = position.coords.longitude;
+    var latitudeElm = document.getElementById("latitude");
+    latitudeElm.innerText = position.coords.latitude;
+
+}
